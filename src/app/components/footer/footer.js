38af29f0435ce4f923e16086cf204f30545e05de
Barry-san <oyeyemimubarak6@gmail.com>
2023-11-09 20:00:26 +0100

    created the footer component

---
+++ src/app/components/footer/footer.js
@@ -0,0 +1,40 @@
+import Image from "next/image";
+
+export default function Footer() {
+  return (
+    <div className="w-full bg-primary-0 flex justify-between p-4">
+      <div>
+        <p>&copy; 2023 Powered by Marvel</p>
+      </div>
+      <div className="flex gap-4">
+        <a href="https://github.com/barry-san" target="_blank">
+          <Image
+            src="/github.svg"
+            alt="github logo"
+            title="github"
+            width={30}
+            height={30}
+          />
+        </a>
+        <a href="https://twitter.com/barry9ja" target="_blank">
+          <Image
+            src="/twitter.svg"
+            alt="twitter logo"
+            title="twitter"
+            width={30}
+            height={30}
+          />
+        </a>
+        <a href="https://linkedin.com" target="_blank">
+          <Image
+            src="/linkedin.svg"
+            alt="linkedin logo"
+            title="linkedin"
+            width={30}
+            height={30}
+          />
+        </a>
+      </div>
+    </div>
+  );
+}
