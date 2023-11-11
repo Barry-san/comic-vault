b1adbfb852b3fcf928c533f34d69494e704423e1
Barry-san <oyeyemimubarak6@gmail.com>
2023-11-11 12:52:42 +0100

    fixed cors issue

--- src/app/hooks/useFetchComics.js
+++ src/app/hooks/useFetchComics.js
@@ -5,7 +5,6 @@ export default function useFetchComics(endpoint = "") {
   const [comics, setComics] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
   useEffect(() => {
-    console.log(process.env.NEXT_PUBLIC_BASE_URL);
     try {
       fetch(
         `${process.env.NEXT_PUBLIC_BASE_URL}${endpoint}?ts=1&apikey=${process.env.NEXT_PUBLIC_API_KEY}&hash=${process.env.NEXT_PUBLIC_MD5_HASH}`,
