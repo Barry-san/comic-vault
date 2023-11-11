7904e77b15473cf6f43f8143151dbdece86d8281
Barry-san <oyeyemimubarak6@gmail.com>
2023-11-11 12:50:13 +0100

    added a top border to the footer

--- src/app/components/footer/footer.js
+++ src/app/components/footer/footer.js
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Footer() {
   return (
-    <div className="w-full bg-primary-0 flex justify-between p-4">
+    <div className="w-full bg-primary-0 flex justify-between p-4 border-t">
       <div>
         <p>&copy; 2023 Powered by Marvel</p>
       </div>
