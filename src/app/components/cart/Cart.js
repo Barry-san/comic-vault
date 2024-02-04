2e786bd1245d42bca4bba3d83d3ffa8fde736afc
Barry-san <oyeyemimubarak6@gmail.com>
2024-02-04 13:09:29 +0100

    fixed cart open and close behavior and improved navigation logic:

--- src/app/components/cart/Cart.js
+++ src/app/components/cart/Cart.js
@@ -3,20 +3,18 @@ import { useCartContext } from "@/app/context/cartContext";
 import Link from "next/link";
 import Image from "next/image";
 import CartItem from "./CartItem";
-import { close } from "../navigation/toggle";
 
-export default function Cart({ navRef }) {
+export default function Cart({ isOpen, toggleCart }) {
   const { cart, decreaseItem, increaseItem } = useCartContext();
 
   return (
     <div
-      className="h-screen w-full md:w-1/2 fixed right-0 top-0  bg-[#6C1D12] transition-transform translate-x-full p-4 
-      overflow-y-scroll "
-      ref={navRef}
-      aria-expanded={"false"}
-      onBlure={() => close(navRef)}
+      className={`h-screen w-full md:w-1/2 fixed right-0 top-0  bg-[#6C1D12] transition-transform p-4 overflow-y-scroll  ${
+        isOpen ? " translate-x-0" : "translate-x-full"
+      }`}
+      aria-expanded={isOpen}
     >
-      <button onClick={() => close(navRef)} className="">
+      <button onClick={() => toggleCart(!isOpen)} className="">
         <span className="sr-only">close</span>
         <Image
           src={"/close.svg"}
