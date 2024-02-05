c4360c7e2719504ad66d92b0aa08401a01a858c4
Barry-san <oyeyemimubarak6@gmail.com>
2024-02-05 18:59:12 +0100

    fixed eslint apostrophe rule issue

--- src/app/components/hero/hero.js
+++ src/app/components/hero/hero.js
@@ -6,9 +6,9 @@ export default function Hero() {
         <p className="md:w-1/2 text-lg p-4 mx-auto ">
           Dive into the Marvel Universe, where legendary heroes like Spider-Man,
           Iron Man, and Captain America come to life in epic tales of bravery.
-          Whether you're a longtime fan or a newcomer, our curated collection
-          offers something for everyone, allowing you to own a piece of Marvel
-          history and experience the magic of these iconic characters.
+          Whether you&apos;re a longtime fan or a newcomer, our curated
+          collection offers something for everyone, allowing you to own a piece
+          of Marvel history and experience the magic of these iconic characters.
         </p>
       </div>
     </section>
